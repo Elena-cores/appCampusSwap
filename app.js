695b571a6a1bebaf72f3efbef0c8207adb9db405
Elena Cores <55955317+Elena-cores@users.noreply.github.com>
2024-09-27 12:47:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Rutas
-app.use('/index', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
