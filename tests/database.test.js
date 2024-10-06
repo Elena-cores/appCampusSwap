3e23cca9246ed9229059e0cbadd9dfe8d5fedc35
Elena Cores <elenayunxin@gmail.com>
2024-10-06 22:18:18 +0200

    test 1 conexion DB Pass

--- tests/database.test.js
+++ tests/database.test.js
@@ -9,20 +9,15 @@ const database = rewire('../database.js');
 const pool1 = database.__get__('pool1');
 
 describe('Pruebas de Base de Datos', function () {
-
     it('Debe conectarse correctamente a la base de datos', async function () {
         let conn;
         try {
-            // Forzamos un error para que falle
-            throw new Error('Falla intencional en la conexión');
-            
             conn = await pool1.getConnection();
             assert(conn, 'La conexión a la base de datos debería estar establecida');
         } catch (err) {
-            // El test fallará aquí debido a la excepción forzada
             assert.fail(`Error de conexión a la base de datos: ${err.message}`);
         } finally {
-            if (conn) conn.end(); // Cierra la conexión al final si hay una
+            if (conn) conn.end(); // Cierra la conexión al final
         }
     });
 });
