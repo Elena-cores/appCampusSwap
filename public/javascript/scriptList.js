6ce3a94b718c129a13af3f63056ce2e8bc16ec88
Elena Cores <elenayunxin@gmail.com>
2024-10-12 14:40:51 +0200

    drop down menu profile

---
+++ public/javascript/scriptList.js
@@ -0,0 +1,5 @@
+// Toggle the profile dropdown
+function toggleDropdown() {
+    const dropdown = document.getElementById('profileDropdown');
+    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+}
